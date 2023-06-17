b87b1610f93fbad57c29f989ebf08dbd46e64a40
ibrahmad <ibrahmad18@gmail.com>
2023-06-17 18:28:39 +0100

    stylizing app/base, app/course/home adn scholars/home html files in templates & ignoring somes unecessary files

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from .views import courses, publications, scholarList
 
 urls = urlpatterns = [
     path('', courses, name ='Home'),
-    path('app/scholars/', scholarList, name='scholar_list'),
-    path('app/publications/', publications, name='publications'),
-    path('app/courses/', courses, name='courses')
+    path('scholars/', scholarList, name='scholar_list'),
+    path('publications/', publications, name='publications'),
+    path('courses/', courses, name='courses')
 ]
