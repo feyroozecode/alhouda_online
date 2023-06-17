7b06b237796ce15c97e92a536b8459bc449f32e7
ibrahmad <ibrahmad18@gmail.com>
2023-06-17 06:59:17 +0100

    Importing all views to views.py (SRP) apply

--- app/views.py
+++ app/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Scholar, Publication
+from .views.home_views import HomeViews
+from views.scholar_views import ScholarViews
+from views.publication_views import PublicationViews
 
 # Views
-
-def publications(request):
-    publications = Publication.objects.all().order_by('-date_of_pub')
-    
-    return render(request, 'publications.html', {'publications': publications} )
--- app/views/home_views.py
+++ app/views/home_views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render 
 
 class HomeViews:
-    
     def home(request):
         return render(request, 'publications/home.html')
