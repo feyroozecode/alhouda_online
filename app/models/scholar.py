d49209e990111afbca1466dab67e55608387b605
ibrahmad <ibrahmad18@gmail.com>
2023-06-17 07:25:07 +0100

    adding courses to scholar profiles

--- app/models/scholar.py
+++ app/models/scholar.py
@@ -5,6 +5,7 @@ class Scholar(models.Model):
     description = models.TextField()
     location = models.CharField(max_length=255)
     image = models.ImageField(upload_to='scholars/images/', blank=True, null=True)
-
+    courses = models.ManyToManyField('Course', related_name='scholars')
+    
     def __str__(self):
         return self.name
