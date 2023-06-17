afc694e4f8a2133f929737f77870150dba4bcc33
ibrahmad <ibrahmad18@gmail.com>
2023-06-17 07:28:15 +0100

    adding scholar details views and template with course list

--- app/views/scholar_views.py
+++ app/views/scholar_views.py
@@ -11,3 +11,7 @@ class ScholarViews:
         scholar = get_object_or_404(Scholar.objects.all(),)
         
         return render(request, 'app/scholar/scholar_details.html', {'scholar': scholar})
+
+    def scholar_detail(request, scholar_id):
+        scholar = get_object_or_404(Scholar, id=scholar_id)
+        return render(request, 'app/scholar_detail.html', {'scholar': scholar})
