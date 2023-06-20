fa305fefdb14c2d99dfc7d153f5c0e1da1612b48
ibrahmad <ibrahmad18@gmail.com>
2023-06-20 22:51:51 +0100

    update of course list , applyig style bug fix

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from .views import courses, publications, scholarList, scholar_detail
 urls = urlpatterns = [
     path('', courses, name ='Home'),
     path('scholars/', scholarList, name='scholar_list'),
-    // path('scholar_details/<int:id>', scholar_detail,  name='scholar_details'),
+    # path('scholar_details/<int:id>', scholar_detail,  name='scholar_details'),
     path('publications/', publications, name='publications'),
     path('courses/', courses, name='courses')
 ]
